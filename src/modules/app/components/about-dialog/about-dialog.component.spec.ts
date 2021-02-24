cc5794fe8d1a6f1778feb242b55658d9a3285f88
Thomas Langvann <thomas.langvann@nb.no>
2021-02-24 11:36:57 +0100

    Initial running about-dialog-test

--- src/modules/app/components/about-dialog/about-dialog.component.spec.ts
+++ src/modules/app/components/about-dialog/about-dialog.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { AboutDialogComponent } from './about-dialog.component';
+import { By } from '@angular/platform-browser';
 import {AppConfigService} from '../../../core/services';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import {CoreTestingModule} from '../../../core/core.testing.module';
+import {AppConfig} from '../../../core/models/app-config.model';
 
 describe('AboutDialogComponent', () => {
   let component: AboutDialogComponent;
   let fixture: ComponentFixture<AboutDialogComponent>;
+  let appconf: AppConfig;
+
+  appconf = new AppConfig( {
+    versions: {}
+  });
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      imports: [CoreTestingModule.forRoot(), MatListModule, MatDialogModule],
       declarations: [ AboutDialogComponent ],
-      providers: [AppConfigService]
+      providers: [{ provide: AppConfigService, useValue: appconf}]
     })
     .compileComponents();
   }));
@@ -23,5 +33,12 @@ describe('AboutDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have content in deployment-version-list ', () => {
+    const list = fixture.debugElement.query(By.css('.deployment-version-list')).nativeElement;
+    expect(list.innerHTML).not.toBeNull();
+    // console.log(list.innerHTML);
+    expect(list.innerHTML.length).toBeGreaterThan(0);
   });
 });
