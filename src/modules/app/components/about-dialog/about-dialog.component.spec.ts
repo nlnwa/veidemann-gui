166a0c80250ee6ad82f4a4e9a6dd622606c6e46a
Thomas Langvann <thomas.langvann@nb.no>
2021-02-24 14:29:48 +0100

    More tests, added imports for material-stuff

--- src/modules/app/components/about-dialog/about-dialog.component.spec.ts
+++ src/modules/app/components/about-dialog/about-dialog.component.spec.ts
@@ -2,18 +2,20 @@ import {AboutDialogComponent} from './about-dialog.component';
 import {AppConfigService} from '../../../core/services';
 import {createComponentFactory, Spectator} from '@ngneat/spectator';
 import {DeploymentVersions} from '../../../../shared/models/deployment-versions.model';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
 
 describe('AboutDialogComponent', () => {
   let spectator: Spectator<AboutDialogComponent>;
   let versions: DeploymentVersions;
 
-  // TODO: Hvorfor er dette nødvendig?
-  versions = new DeploymentVersions({});
+  const expectedVersion = 'unittesting';
+  versions = new DeploymentVersions({veidemannController: expectedVersion});
 
   const createComponent = createComponentFactory(
     {
       component: AboutDialogComponent,
-      imports: [],
+      imports: [MatDialogModule, MatDividerModule],
       providers: [{provide: AppConfigService, useValue: {versions}}]
     });
 
@@ -21,5 +23,15 @@ describe('AboutDialogComponent', () => {
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('should use dashboard-version from environment', () => {
+    expect(spectator.component.dashBoardversion).toBe('DEV');
+  });
+
+  it('should have expected content in deployment-version-list ', () => {
+    const list = spectator.queryAll('.deployment-version-list li');
+    expect(list).not.toBeNull();
+    expect(list).toHaveText(expectedVersion);
   });
 });
