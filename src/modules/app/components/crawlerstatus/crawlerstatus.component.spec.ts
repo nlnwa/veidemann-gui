2e5ba653d6fd497b89f0b03a3285666031ac7e10
Thomas Langvann <thomas.langvann@nb.no>
2021-02-24 15:18:44 +0100

    Running tests with spectator

--- src/modules/app/components/crawlerstatus/crawlerstatus.component.spec.ts
+++ src/modules/app/components/crawlerstatus/crawlerstatus.component.spec.ts
@@ -1,38 +1,31 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CrawlerStatusComponent} from './crawlerstatus.component';
 import {MatCardModule} from '@angular/material/card';
 import {RunStatus} from '../../../../shared/models/controller';
 import {AbilityModule} from '@casl/angular';
 import {CoreTestingModule} from '../../../core/core.testing.module';
-import {By} from '@angular/platform-browser';
+import {createComponentFactory, Spectator} from '@ngneat/spectator';
 
 describe('CrawlerStatusComponent', () => {
-  let component: CrawlerStatusComponent;
-  let fixture: ComponentFixture<CrawlerStatusComponent>;
+  let spectator: Spectator<CrawlerStatusComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  const createComponent = createComponentFactory(
+    {
+      component: CrawlerStatusComponent,
       imports: [CoreTestingModule.forRoot(), MatCardModule, AbilityModule],
-      declarations: [ CrawlerStatusComponent ],
       providers: []
-    })
-    .compileComponents();
-  }));
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CrawlerStatusComponent);
-    component = fixture.componentInstance;
-    component.runStatus = RunStatus.RUNNING;
-    fixture.detectChanges();
-  });
+  beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 
-  it('should have status in html', () => {
-    const list = fixture.debugElement.query(By.css('.crawlerRunningButton')).nativeElement;
-    expect(list.innerHTML).toContain('RUNNING');
+  it('should have correct status in html', () => {
+    spectator.setInput('runStatus', RunStatus.RUNNING);
+    const list = spectator.queryAll('.crawlerRunningButton');
+    expect(list).not.toBeNull();
+    expect(list).toHaveText('RUNNING');
   });
 
 });
