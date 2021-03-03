4ab3ae0fd3eb20b554755d9264c85cd36aa075f9
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 11:06:05 +0100

    Fixed imports (abilities, coretestingmodule)

--- src/modules/config/components/annotation/annotation.component.spec.ts
+++ src/modules/config/components/annotation/annotation.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AnnotationComponent } from './annotation.component';
 import {FormBuilder} from '@angular/forms';
+import {AuthService} from '../../../core/services/auth';
+import {CoreTestingModule} from '../../../core/core.testing.module';
+import {AbilityModule} from '@casl/angular';
 
 describe('AnnotationComponent', () => {
   let component: AnnotationComponent;
@@ -9,7 +12,16 @@ describe('AnnotationComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
+      imports: [CoreTestingModule.forRoot(), AbilityModule],
+      providers: [FormBuilder,
+        {
+          provide: AuthService,
+          useValue: {
+            isAdmin: () => true,
+            canUpdate: () => true,
+          }
+        }
+          ],
       declarations: [ AnnotationComponent ]
     })
     .compileComponents();
