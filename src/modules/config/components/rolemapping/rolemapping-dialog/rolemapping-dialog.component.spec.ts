7a77b3676d3dbb72eaa7b1f6442d8febfd6964bd
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 13:26:05 +0100

    Added configobject

--- src/modules/config/components/rolemapping/rolemapping-dialog/rolemapping-dialog.component.spec.ts
+++ src/modules/config/components/rolemapping/rolemapping-dialog/rolemapping-dialog.component.spec.ts
@@ -1,14 +1,37 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { RoleMappingDialogComponent } from './rolemapping-dialog.component';
+import {RoleMappingDialogComponent} from './rolemapping-dialog.component';
+import {FormBuilder} from '@angular/forms';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject, Kind} from '../../../../../shared/models/config';
+import {MatError} from '@angular/material/form-field';
+import {CommonsModule} from '../../../../commons';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-describe('RoleMappingDialogComponent', () => {
+fdescribe('RoleMappingDialogComponent', () => {
   let component: RoleMappingDialogComponent;
   let fixture: ComponentFixture<RoleMappingDialogComponent>;
 
+  // TODO: Use ConfigDialogData
+  // FIXME: unødvendig initialisering
+  const MY_CONF = {
+    configObject: new ConfigObject(
+      {
+        kind: Kind.ROLEMAPPING,
+      }),
+    options: [],
+    allSelected: false
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoleMappingDialogComponent ]
+      imports: [CoreTestingModule.forRoot(), MatDialogModule, CommonsModule, NoopAnimationsModule],
+      declarations: [ RoleMappingDialogComponent, MatError ],
+      providers: [FormBuilder,
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
     .compileComponents();
   }));
