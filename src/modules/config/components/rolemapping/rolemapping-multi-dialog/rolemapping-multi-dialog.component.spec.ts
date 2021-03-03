41dcdb48a962231e5d598586ea99bcb93b9278db
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 13:22:41 +0100

    Added configobject

--- src/modules/config/components/rolemapping/rolemapping-multi-dialog/rolemapping-multi-dialog.component.spec.ts
+++ src/modules/config/components/rolemapping/rolemapping-multi-dialog/rolemapping-multi-dialog.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RoleMappingMultiDialogComponent } from './rolemapping-multi-dialog.component';
+import {FormBuilder} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject, Kind} from '../../../../../shared/models/config';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonsModule} from '../../../../commons';
 
 describe('RoleMappingMultiDialogComponent', () => {
   let component: RoleMappingMultiDialogComponent;
   let fixture: ComponentFixture<RoleMappingMultiDialogComponent>;
 
+  // TODO: Use ConfigDialogData
+  // FIXME: Unødvenig initialisering?
+  const MY_CONF = {
+    configObject: new ConfigObject(
+      {
+        kind: Kind.ROLEMAPPING,
+      }),
+    options: [],
+    allSelected: false
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoleMappingMultiDialogComponent ]
+      imports: [CommonsModule, NoopAnimationsModule],
+      declarations: [ RoleMappingMultiDialogComponent ],
+      providers: [FormBuilder,
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF}
+      ]
     })
     .compileComponents();
   }));
