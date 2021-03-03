6a6a3d65970768cd4bed013b49aec4aaea2f7ce2
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 13:42:39 +0100

    Fix imports

--- src/modules/config/components/label/label-multi/label-multi.component.spec.ts
+++ src/modules/config/components/label/label-multi/label-multi.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LabelMultiComponent } from './label-multi.component';
-import {LabelService} from "../../../services";
+import {LabelService} from '../../../services';
+import {ConfigObject} from '../../../../../shared/models/config';
+import {MaterialModule} from '../../../../commons/material.module';
 
-describe('LabelMultiComponent', () => {
+fdescribe('LabelMultiComponent', () => {
   let component: LabelMultiComponent;
   let fixture: ComponentFixture<LabelMultiComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [LabelService],
-      declarations: [ LabelMultiComponent ]
+      imports: [MaterialModule],
+      declarations: [ LabelMultiComponent ],
+      providers: [
+        {
+          provide: LabelService,
+          useValue: {}
+        }
+      ]
     })
     .compileComponents();
   }));
@@ -18,6 +26,7 @@ describe('LabelMultiComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LabelMultiComponent);
     component = fixture.componentInstance;
+    component.configObject = new ConfigObject();
     fixture.detectChanges();
   });
 
