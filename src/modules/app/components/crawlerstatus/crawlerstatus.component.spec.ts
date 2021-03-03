395028f8f870afa6e4a79791f97d96e46c35a192
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 10:48:19 +0100

    more tests

--- src/modules/app/components/crawlerstatus/crawlerstatus.component.spec.ts
+++ src/modules/app/components/crawlerstatus/crawlerstatus.component.spec.ts
@@ -21,11 +21,27 @@ describe('CrawlerStatusComponent', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  it('should have correct status in html', () => {
+  it('should show RUNNING status in html', () => {
     spectator.setInput('runStatus', RunStatus.RUNNING);
     const list = spectator.queryAll('.crawlerRunningButton');
     expect(list).not.toBeNull();
     expect(list).toHaveText('RUNNING');
+  });
+
+  it('should show PAUSED status in html', () => {
+    spectator.setInput('runStatus', RunStatus.PAUSED);
+    const list = spectator.queryAll('.crawlerPausedButton');
+    expect(list).not.toBeNull();
+    expect(list).toHaveText('PAUSED');
+  });
+
+
+  fit('should show PAUSE_REQUESTED in html', () => {
+    spectator.setInput('runStatus', RunStatus.PAUSE_REQUESTED);
+    //FIXME: Fragile testing based on generic selector
+    const list = spectator.queryAll('h1');
+    expect(list).not.toBeNull();
+    expect(list).toHaveText('IS PAUSING');
   });
 
 });
