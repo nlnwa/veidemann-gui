1582613d16a8c946012266ed01bbb0b490497f80
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 13:31:26 +0100

    Added configobject and imports

--- src/modules/config/components/politenessconfig/politenessconfig-preview/politenessconfig-preview.component.spec.ts
+++ src/modules/config/components/politenessconfig/politenessconfig-preview/politenessconfig-preview.component.spec.ts
@@ -1,14 +1,25 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { PolitenessconfigPreviewComponent } from './politenessconfig-preview.component';
+import {PolitenessconfigPreviewComponent} from './politenessconfig-preview.component';
+import {DurationFormatPipe} from '../../../../commons/pipes/duration-format.pipe';
+import {ConfigObject, Kind, PolitenessConfig} from '../../../../../shared/models/config';
+import {MatLabel} from '@angular/material/form-field';
+import {MatChipList} from '@angular/material/chips';
 
 describe('PolitenessconfigPreviewComponent', () => {
   let component: PolitenessconfigPreviewComponent;
   let fixture: ComponentFixture<PolitenessconfigPreviewComponent>;
 
+  // FIXME: Unødvendig?
+  const MY_CONF = new ConfigObject({
+    kind: Kind.POLITENESSCONFIG,
+    politenessConfig: new PolitenessConfig()
+  });
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PolitenessconfigPreviewComponent ]
+      imports: [],
+      declarations: [ PolitenessconfigPreviewComponent, DurationFormatPipe, MatLabel, MatChipList],
+      providers: []
     })
     .compileComponents();
   }));
@@ -16,6 +27,7 @@ describe('PolitenessconfigPreviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PolitenessconfigPreviewComponent);
     component = fixture.componentInstance;
+    component.configObject = MY_CONF;
     fixture.detectChanges();
   });
 
