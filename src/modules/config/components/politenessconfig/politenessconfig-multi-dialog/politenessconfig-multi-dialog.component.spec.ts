17c19a097dbe2cc8a3d90637fcbcddfb44517007
Thomas Langvann <thomas.langvann@nb.no>
2021-03-03 13:34:26 +0100

    Added configobject

--- src/modules/config/components/politenessconfig/politenessconfig-multi-dialog/politenessconfig-multi-dialog.component.spec.ts
+++ src/modules/config/components/politenessconfig/politenessconfig-multi-dialog/politenessconfig-multi-dialog.component.spec.ts
@@ -1,18 +1,43 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { PolitenessConfigMultiDialogComponent } from './politenessconfig-multi-dialog.component';
+import {PolitenessConfigMultiDialogComponent} from './politenessconfig-multi-dialog.component';
 import {FormBuilder} from '@angular/forms';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigDialogData} from '../../../func';
+import {ConfigObject, Kind} from '../../../../../shared/models/config';
+import {AuthService} from '../../../../core/services/auth';
+import {DurationPickerComponent} from '../..';
+import {MatLabel} from '@angular/material/form-field';
+import {CommonsModule} from '../../../../commons';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {LabelMultiComponent} from '../../label/label-multi/label-multi.component';
+import {LabelService} from '../../../services';
 
-describe('PolitenessConfigMultiDialogComponent', () => {
+fdescribe('PolitenessConfigMultiDialogComponent', () => {
   let component: PolitenessConfigMultiDialogComponent;
   let fixture: ComponentFixture<PolitenessConfigMultiDialogComponent>;
 
+  // FIXME: Unødvendig?
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({
+      kind: Kind.POLITENESSCONFIG
+    }),
+    options: {robotsPolicies: []}
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ PolitenessConfigMultiDialogComponent ]
+      imports: [CoreTestingModule.forRoot(), MatDialogModule, CommonsModule, NoopAnimationsModule],
+      declarations: [PolitenessConfigMultiDialogComponent, DurationPickerComponent, MatLabel, LabelMultiComponent],
+      providers: [FormBuilder,
+        {provide: LabelService, useValue: {}},
+        {provide: AuthService, useValue: {canUpdate: () => true}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
