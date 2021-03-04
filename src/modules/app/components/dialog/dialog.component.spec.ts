f41e62bcc0a69a11d9e98a8d441a4e0c4e8d3c85
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 15:06:58 +0100

    Added commons

--- src/modules/app/components/dialog/dialog.component.spec.ts
+++ src/modules/app/components/dialog/dialog.component.spec.ts
@@ -1,15 +1,18 @@
 import {MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
 import {DialogComponent} from './dialog.component';
 import {createComponentFactory, Spectator} from '@ngneat/spectator';
+import {CommonsModule} from '../../../commons';
 
-describe('DialogComponent', () => {
+fdescribe('DialogComponent', () => {
   let spectator: Spectator<DialogComponent>;
 
   const createComponent = createComponentFactory(
     {
       component: DialogComponent,
-      imports: [MatDialogModule],
-      providers: [{provide: MAT_DIALOG_DATA, useValue: {}}]
+      imports: [CommonsModule, MatDialogModule],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
     });
 
   beforeEach(() => spectator = createComponent());
