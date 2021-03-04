041f785dcf6680cf3a1c6acb7e066cc55866f28d
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 17:49:31 +0100

    Added pipes with dependencies (services mocked)

--- src/modules/report/components/crawl-execution-status/crawl-execution-status.component.spec.ts
+++ src/modules/report/components/crawl-execution-status/crawl-execution-status.component.spec.ts
@@ -1,8 +1,14 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {CrawlExecutionStatusComponent} from './crawl-execution-status.component';
 import {CommonsModule} from '../../../commons';
-import {ReportModule} from '../../report.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import {CrawlExecutionStatus} from '../../../../shared/models';
+import {CrawlExecutionService, JobExecutionService} from '../../services';
+import {of} from 'rxjs';
+import {JobNamePipe, SeedNamePipe} from '../../pipe';
+import {CoreTestingModule} from '../../../core/core.testing.module';
+
 
 describe('CrawlExecutionStatusComponent', () => {
   let component: CrawlExecutionStatusComponent;
@@ -10,15 +16,28 @@ describe('CrawlExecutionStatusComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [ReportModule],
-      declarations: [ CrawlExecutionStatusComponent ]
+      imports: [RouterTestingModule, CommonsModule, CoreTestingModule.forRoot()],
+      declarations: [CrawlExecutionStatusComponent, SeedNamePipe, JobNamePipe],
+      providers: [
+        {
+          provide: JobExecutionService, useValue: {
+            getJob: () => of(null)
+          }
+        },
+        {
+          provide: CrawlExecutionService, useValue: {
+            getSeed: () => of(null)
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CrawlExecutionStatusComponent);
     component = fixture.componentInstance;
+    component.crawlExecutionStatus = new CrawlExecutionStatus();
     fixture.detectChanges();
   });
 
