b58a19c093f7d3dde82eb0f0264b7ce667796412
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 12:31:26 +0100

    Added configdialog and imports

--- src/modules/config/components/crawljobs/crawljob-multi-dialog/crawljobs-multi-dialog.component.spec.ts
+++ src/modules/config/components/crawljobs/crawljob-multi-dialog/crawljobs-multi-dialog.component.spec.ts
@@ -1,18 +1,53 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { CrawlJobMultiDialogComponent } from './crawljobs-multi-dialog.component';
+import {CrawlJobMultiDialogComponent} from './crawljobs-multi-dialog.component';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigDialogData} from '../../../func';
+import {ConfigObject, Kind} from '../../../../../shared/models';
+import {AuthService} from '../../../../core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {FilesizeInputComponent} from '../../filesize-input/filesize-input.component';
+import {DurationPickerComponent} from '../../durationpicker/duration-picker';
+import {LabelMultiComponent} from '../../label/label-multi/label-multi.component';
 import {FormBuilder} from '@angular/forms';
+import {CommonsModule} from '../../../../commons';
+import {LabelService} from '../../../services';
+import {CrawlJobDetailsComponent} from '../crawljob-details/crawl-job-details.component';
 
 describe('CrawlJobMultiDialogComponent', () => {
   let component: CrawlJobMultiDialogComponent;
   let fixture: ComponentFixture<CrawlJobMultiDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({kind: Kind.CRAWLJOB}),
+    options: {},
+  };
+
+  // TODO: Reduce imports
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ CrawlJobMultiDialogComponent ]
+      imports: [CommonsModule,
+        MatDialogModule,
+        MatSlideToggleModule,
+        MatOptionModule, MatSelectModule, NoopAnimationsModule, CoreTestingModule.forRoot()],
+      declarations: [CrawlJobMultiDialogComponent,
+        CrawlJobDetailsComponent,
+        FilesizeInputComponent,
+        DurationPickerComponent,
+        LabelMultiComponent],
+      providers: [
+        FormBuilder,
+        {provide: LabelService, useValue: {}},
+        {provide: AuthService, useValue: {canUpdate: () => true}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}},
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
