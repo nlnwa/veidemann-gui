89b92025f13558b1d956eba8392ecf2ba89741f9
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 16:02:55 +0100

    Add config and imports

--- src/modules/config/components/crawlhostgroupconfig/crawlhostgroupconfig-dialog/crawlhostgroupconfig-dialog.component.spec.ts
+++ src/modules/config/components/crawlhostgroupconfig/crawlhostgroupconfig-dialog/crawlhostgroupconfig-dialog.component.spec.ts
@@ -1,18 +1,46 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { CrawlHostGroupConfigDialogComponent } from './crawlhostgroupconfig-dialog.component';
-import {FormBuilder} from '@angular/forms';
+import {CrawlHostGroupConfigDialogComponent} from './crawlhostgroupconfig-dialog.component';
+import {AuthService} from '../../../../core';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject, Kind} from '../../../../../shared/models';
+import {ConfigDialogData} from '../../../func';
+import {CommonsModule} from '../../../../commons';
+import {MetaComponent} from '../../meta/meta.component';
+import {LabelComponent} from '../../label/label.component';
+import {LabelService} from '../../../services';
+import {of} from 'rxjs';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {AnnotationComponent} from '../../annotation/annotation.component';
+import {AbilityModule} from '@casl/angular';
 
 describe('CrawlHostGroupConfigDialogComponent', () => {
   let component: CrawlHostGroupConfigDialogComponent;
   let fixture: ComponentFixture<CrawlHostGroupConfigDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({kind: Kind.CRAWLHOSTGROUPCONFIG}),
+    options: {}
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ CrawlHostGroupConfigDialogComponent ]
+      imports: [AbilityModule, CoreTestingModule.forRoot(), CommonsModule, NoopAnimationsModule],
+      providers: [
+        {
+          provide: LabelService,
+          useValue: {
+            getLabelKeys: () => of([])
+          }
+        },
+        {provide: AuthService, useValue: {canUpdate: () => true}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}},
+      ],
+      declarations: [CrawlHostGroupConfigDialogComponent, MetaComponent, LabelComponent, AnnotationComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
