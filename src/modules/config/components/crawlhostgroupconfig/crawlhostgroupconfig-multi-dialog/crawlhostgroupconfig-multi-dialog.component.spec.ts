6585e055c0fe9ead52b8b81ebfbd57567e8c97f0
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 13:52:00 +0100

    Added imports and config

--- src/modules/config/components/crawlhostgroupconfig/crawlhostgroupconfig-multi-dialog/crawlhostgroupconfig-multi-dialog.component.spec.ts
+++ src/modules/config/components/crawlhostgroupconfig/crawlhostgroupconfig-multi-dialog/crawlhostgroupconfig-multi-dialog.component.spec.ts
@@ -1,16 +1,39 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { CrawlHostGroupConfigMultiDialogComponent } from './crawlhostgroupconfig-multi-dialog.component';
+import {CrawlHostGroupConfigMultiDialogComponent} from './crawlhostgroupconfig-multi-dialog.component';
+import {FormBuilder} from '@angular/forms';
+import {AuthService} from '../../../../core';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject} from '../../../../../shared/models';
+import {ConfigDialogData} from '../../../func';
+import {LabelMultiComponent} from '../../label/label-multi/label-multi.component';
+import {LabelService} from '../../../services';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipList} from '@angular/material/chips';
 
 describe('CrawlHostGroupConfigMultiDialogComponent', () => {
   let component: CrawlHostGroupConfigMultiDialogComponent;
   let fixture: ComponentFixture<CrawlHostGroupConfigMultiDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject(),
+    options: {}
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CrawlHostGroupConfigMultiDialogComponent ]
+      imports: [CoreTestingModule.forRoot(), MatDialogModule, MatIconModule, MatButtonToggleModule],
+      declarations: [CrawlHostGroupConfigMultiDialogComponent, LabelMultiComponent, MatChipList],
+      providers: [FormBuilder,
+        {provide: LabelService, useValue: {}},
+        {provide: AuthService, useValue: {canUpdate: () => true }},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
