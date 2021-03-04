7eb8ee0ed291e9091f5eebdcc0b27e461dab6a7f
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 13:32:38 +0100

    More tests

--- src/modules/config/components/shortcut/filter-shortcut/filter-shortcut.component.spec.ts
+++ src/modules/config/components/shortcut/filter-shortcut/filter-shortcut.component.spec.ts
@@ -1,7 +1,7 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {FilterShortcutComponent} from './filter-shortcut.component';
-import {ConfigObject, Kind} from '../../../../../shared/models/config';
+import {ConfigObject, Kind} from '../../../../../shared/models';
 import {CoreTestingModule} from '../../../../core/core.testing.module';
 import {AbilityModule} from '@casl/angular';
 import {MatListModule} from '@angular/material/list';
@@ -22,13 +22,29 @@ describe('FilterShortcutComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FilterShortcutComponent);
     component = fixture.componentInstance;
-    // FIXME: Burde ikke være nødvendig
-    // Er grunnet ability-implementasjonen
-    component.configObject = new ConfigObject({kind: Kind.SEED});
+    component.configObject = new ConfigObject();
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    component.configObject = new ConfigObject({kind: Kind.CRAWLENTITY});
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  it('should create with SEED config', () => {
+    component.configObject = new ConfigObject({kind: Kind.SEED});
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+  it('should create with CRAWLJOB config', () => {
+    component.configObject = new ConfigObject({kind: Kind.CRAWLJOB});
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
