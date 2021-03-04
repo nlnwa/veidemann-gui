fe36d4ed2e0dacb13eb7a3c88d4913b14d9c348c
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 14:10:58 +0100

    Added imports and config

--- src/modules/config/components/crawlconfig/crawlconfig-multi-dialog/crawlconfig-multi-dialog.component.spec.ts
+++ src/modules/config/components/crawlconfig/crawlconfig-multi-dialog/crawlconfig-multi-dialog.component.spec.ts
@@ -1,16 +1,39 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { CrawlConfigMultiDialogComponent } from './crawlconfig-multi-dialog.component';
-import {FormBuilder} from "@angular/forms";
+import {CrawlConfigMultiDialogComponent} from './crawlconfig-multi-dialog.component';
+import {FormBuilder} from '@angular/forms';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject, Kind} from '../../../../../shared/models';
+import {ConfigDialogData} from '../../../func';
+import {AuthService} from '../../../../core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {LabelMultiComponent} from '../../label/label-multi/label-multi.component';
+import {LabelService} from '../../../services';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
 
+// TODO-Minor: Mat-form-field
 describe('CrawlConfigMultiDialogComponent', () => {
   let component: CrawlConfigMultiDialogComponent;
   let fixture: ComponentFixture<CrawlConfigMultiDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({kind: Kind.CRAWLCONFIG}),
+    options: {}
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ CrawlConfigMultiDialogComponent ]
+      imports: [CoreTestingModule.forRoot(), MatDialogModule, MatCheckboxModule, MatChipsModule, MatIconModule, MatButtonToggleModule],
+      declarations: [ CrawlConfigMultiDialogComponent, LabelMultiComponent ],
+      providers: [FormBuilder,
+        {provide: LabelService, useValue: {}},
+        {provide: AuthService, useValue: {canUpdate: () => true}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}},
+      ]
     })
     .compileComponents();
   }));
