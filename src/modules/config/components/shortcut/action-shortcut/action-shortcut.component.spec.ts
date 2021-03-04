90a5a42afd265ed3f43262a3f45d78625f5cc7ef
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 13:28:11 +0100

    More tests

--- src/modules/config/components/shortcut/action-shortcut/action-shortcut.component.spec.ts
+++ src/modules/config/components/shortcut/action-shortcut/action-shortcut.component.spec.ts
@@ -1,7 +1,7 @@
 import {ActionShortcutComponent} from './action-shortcut.component';
 import {AbilityModule} from '@casl/angular';
 import {CoreTestingModule} from '../../../../core/core.testing.module';
-import {ConfigObject} from '../../../../../shared/models/config';
+import {ConfigObject, Kind} from '../../../../../shared/models';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {MatListModule} from '@angular/material/list';
 
@@ -21,13 +21,35 @@ describe('ActionShortcutComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ActionShortcutComponent);
     component = fixture.componentInstance;
-    // FIXME: Burde ikke være nødvendig
-    // Grunnet ability
     component.configObject = new ConfigObject();
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.configObject = new ConfigObject();
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should create with SEED', () => {
+    component.configObject = new ConfigObject({kind: Kind.SEED});
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+
+  it('should create with CRAWLJOB', () => {
+    component.configObject = new ConfigObject({kind: Kind.CRAWLJOB});
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+
+  it('should create with CRAWLENTITY', () => {
+    component.configObject = new ConfigObject({kind: Kind.CRAWLENTITY});
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+  });
+
+
 });
