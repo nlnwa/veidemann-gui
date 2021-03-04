fde89409fe2a993dc387eb1817535dc381fbaeab
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 14:30:23 +0100

    Added imports and config

--- src/modules/config/components/browserconfig/browserconfig-multi-dialog/browserconfig-multi-dialog.component.spec.ts
+++ src/modules/config/components/browserconfig/browserconfig-multi-dialog/browserconfig-multi-dialog.component.spec.ts
@@ -1,17 +1,50 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BrowserConfigMultiDialogComponent } from './browserconfig-multi-dialog.component';
-import {FormBuilder} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {AuthService} from '../../../../core';
+import {ConfigObject, Kind} from '../../../../../shared/models';
+import {DurationPickerComponent} from '../..';
+import {ConfigDialogData} from '../../../func';
+import {LabelMultiComponent} from '../../label/label-multi/label-multi.component';
+import {LabelService} from '../../../services';
+import {CommonsModule} from '../../../../commons';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-describe('BrowserconfigMultiDialogComponent', () => {
+describe('BrowserConfigMultiDialogComponent', () => {
   let component: BrowserConfigMultiDialogComponent;
   let fixture: ComponentFixture<BrowserConfigMultiDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject(
+      {
+        kind: Kind.BROWSERCONFIG
+      }),
+    options: {},
+    allSelected: false
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ BrowserConfigMultiDialogComponent ]
-      declarations: [ BrowserConfigMultiDialogComponent ]
+      imports: [CommonsModule, NoopAnimationsModule],
+      declarations: [ BrowserConfigMultiDialogComponent, DurationPickerComponent, LabelMultiComponent],
+      providers: [
+        {provide: LabelService, useValue: {}},
+        {
+          provide: AuthService,
+          useValue: {
+            isAdmin: () => true,
+            canUpdate: () => true,
+          }
+        },
+        { provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: () => {
+            }
+          }
+        }]
     })
     .compileComponents();
   }));
