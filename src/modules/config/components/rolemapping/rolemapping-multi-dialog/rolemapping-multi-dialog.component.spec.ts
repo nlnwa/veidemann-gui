11a50dd438d7b05a75553688083ce268d030cac9
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 13:03:41 +0100

    Simplified conf

--- src/modules/config/components/rolemapping/rolemapping-multi-dialog/rolemapping-multi-dialog.component.spec.ts
+++ src/modules/config/components/rolemapping/rolemapping-multi-dialog/rolemapping-multi-dialog.component.spec.ts
@@ -3,23 +3,18 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RoleMappingMultiDialogComponent } from './rolemapping-multi-dialog.component';
 import {FormBuilder} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {ConfigObject, Kind} from '../../../../../shared/models/config';
+import {ConfigObject, Kind} from '../../../../../shared/models';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {CommonsModule} from '../../../../commons';
+import {ConfigDialogData} from '../../../func';
 
 describe('RoleMappingMultiDialogComponent', () => {
   let component: RoleMappingMultiDialogComponent;
   let fixture: ComponentFixture<RoleMappingMultiDialogComponent>;
 
-  // TODO: Use ConfigDialogData
-  // FIXME: Unødvenig initialisering?
-  const MY_CONF = {
-    configObject: new ConfigObject(
-      {
-        kind: Kind.ROLEMAPPING,
-      }),
-    options: [],
-    allSelected: false
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({kind: Kind.ROLEMAPPING}),
+    options: {}
   };
 
   beforeEach(waitForAsync(() => {
