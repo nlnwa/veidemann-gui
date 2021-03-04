d2abbff10399f3b00043bd500c0cf8095d89fcd8
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 15:52:45 +0100

    Add config and imports

--- src/modules/config/components/crawlconfig/crawlconfig-dialog/crawlconfig-dialog.component.spec.ts
+++ src/modules/config/components/crawlconfig/crawlconfig-dialog/crawlconfig-dialog.component.spec.ts
@@ -1,18 +1,46 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { CrawlConfigDialogComponent } from './crawlconfig-dialog.component';
-import {FormBuilder} from '@angular/forms';
+import {CrawlConfigDialogComponent} from './crawlconfig-dialog.component';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject, Kind} from '../../../../../shared/models';
+import {ConfigDialogData} from '../../../func';
+import {AuthService} from '../../../../core';
+import {CommonsModule} from '../../../../commons';
+import {MetaComponent} from '../../meta/meta.component';
+import {LabelComponent} from '../../label/label.component';
+import {LabelService} from '../../../services';
+import {of} from 'rxjs';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {AnnotationComponent} from '../../annotation/annotation.component';
+import {AbilityModule} from '@casl/angular';
 
 describe('CrawlConfigDialogComponent', () => {
   let component: CrawlConfigDialogComponent;
   let fixture: ComponentFixture<CrawlConfigDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({kind: Kind.CRAWLCONFIG}),
+    options: {}
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ CrawlConfigDialogComponent ]
+      imports: [AbilityModule, CoreTestingModule.forRoot(), CommonsModule, NoopAnimationsModule],
+      declarations: [CrawlConfigDialogComponent, MetaComponent, LabelComponent, AnnotationComponent],
+      providers: [
+        {
+          provide: LabelService,
+          useValue: {
+            getLabelKeys: () => of([])
+          }
+        },
+        {provide: AuthService, useValue: {canUpdate: () => true}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
