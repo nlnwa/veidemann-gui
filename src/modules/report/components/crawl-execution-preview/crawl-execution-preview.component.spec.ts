8197a240459d89323d056c666395dabb811b80a3
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 13:43:42 +0100

    Add imports and config

--- src/modules/report/components/crawl-execution-preview/crawl-execution-preview.component.spec.ts
+++ src/modules/report/components/crawl-execution-preview/crawl-execution-preview.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CrawlExecutionPreviewComponent } from './crawl-execution-preview.component';
+import {CrawlExecutionStatus} from '../../../../shared/models';
+import {NgxFilesizeModule} from 'ngx-filesize';
+import {ChartsModule} from 'ng2-charts';
+import {MatCardModule} from '@angular/material/card';
 
 describe('CrawlExecutionPreviewComponent', () => {
   let component: CrawlExecutionPreviewComponent;
@@ -8,7 +12,8 @@ describe('CrawlExecutionPreviewComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CrawlExecutionPreviewComponent ]
+      declarations: [ CrawlExecutionPreviewComponent ],
+      imports: [NgxFilesizeModule, ChartsModule, MatCardModule]
     })
     .compileComponents();
   }));
@@ -16,6 +21,7 @@ describe('CrawlExecutionPreviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CrawlExecutionPreviewComponent);
     component = fixture.componentInstance;
+    component.crawlExecutionStatus = new CrawlExecutionStatus();
     fixture.detectChanges();
   });
 
