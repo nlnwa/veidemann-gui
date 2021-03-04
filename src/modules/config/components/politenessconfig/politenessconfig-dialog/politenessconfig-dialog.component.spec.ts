c8eb3b7f5ce7108e52a9bd14afd6155096757975
Thomas Langvann <thomas.langvann@nb.no>
2021-03-04 14:59:56 +0100

    Added imports and config

--- src/modules/config/components/politenessconfig/politenessconfig-dialog/politenessconfig-dialog.component.spec.ts
+++ src/modules/config/components/politenessconfig/politenessconfig-dialog/politenessconfig-dialog.component.spec.ts
@@ -1,18 +1,57 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { PolitenessConfigDialogComponent } from './politenessconfig-dialog.component';
+import {PolitenessConfigDialogComponent} from './politenessconfig-dialog.component';
 import {FormBuilder} from '@angular/forms';
+import {CoreTestingModule} from '../../../../core/core.testing.module';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigDialogData} from '../../../func';
+import {ConfigObject, Kind} from '../../../../../shared/models';
+import {AuthService} from '../../../../core';
+import {
+  AnnotationComponent,
+  DurationPickerComponent,
+  LabelComponent,
+  MetaComponent,
+  SelectorComponent
+} from '../..';
+import {CommonsModule} from '../../../../commons';
+import {LabelService} from '../../../services';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {of} from 'rxjs';
+import {AbilityModule} from '@casl/angular';
 
 describe('PolitenessConfigDialogComponent', () => {
   let component: PolitenessConfigDialogComponent;
   let fixture: ComponentFixture<PolitenessConfigDialogComponent>;
 
+  const MY_CONF: ConfigDialogData = {
+    configObject: new ConfigObject({
+      kind: Kind.POLITENESSCONFIG
+    }),
+    options: {}
+  };
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [FormBuilder],
-      declarations: [ PolitenessConfigDialogComponent ]
+      imports: [AbilityModule, CoreTestingModule.forRoot(), MatDialogModule, CommonsModule, NoopAnimationsModule],
+      declarations: [PolitenessConfigDialogComponent,
+        DurationPickerComponent,
+        SelectorComponent,
+        LabelComponent,
+        MetaComponent,
+        AnnotationComponent],
+      providers: [FormBuilder,
+        {
+          provide: LabelService,
+          useValue: {
+            getLabelKeys: () => of([])
+          }
+        },
+        {provide: AuthService, useValue: {canUpdate: () => true}},
+        {provide: MAT_DIALOG_DATA, useValue: MY_CONF},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
