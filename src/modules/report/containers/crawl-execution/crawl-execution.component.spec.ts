915461685fa646bde191256141660332739e1c8d
Thomas Langvann <thomas.langvann@nb.no>
2021-03-05 12:00:55 +0100

    Add imports and fakeActivatedRoute, needs further mocking

--- src/modules/report/containers/crawl-execution/crawl-execution.component.spec.ts
+++ src/modules/report/containers/crawl-execution/crawl-execution.component.spec.ts
@@ -1,16 +1,43 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {CrawlExecutionComponent} from './crawl-execution.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {CoreTestingModule} from '../../../core/core.testing.module';
+import {CrawlExecutionService} from '../../services';
+import {MatDialog} from '@angular/material/dialog';
+import {ActivatedRoute} from '@angular/router';
+import {of} from 'rxjs';
 
+// TODO: Only creates, no data in view, needs further mocking
 describe('CrawlExecutionComponent', () => {
   let component: CrawlExecutionComponent;
   let fixture: ComponentFixture<CrawlExecutionComponent>;
 
+  const fakeActivatedRoute = {
+    snapshot:
+      {
+        data:
+          {options: {}}
+      },
+    queryParamMap: of(
+      {
+        get: () => {},
+        getAll: () => {}
+      }
+    )
+  };
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CrawlExecutionComponent ]
+      imports: [CoreTestingModule.forRoot(), RouterTestingModule],
+      declarations: [CrawlExecutionComponent],
+      providers: [
+        {provide: CrawlExecutionService, useValue: {}},
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute},
+        {provide: MatDialog, useValue: {}}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
